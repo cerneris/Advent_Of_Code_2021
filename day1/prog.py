b8253fdaeaaf44f924935cab46ad262b0ce8acd8
Jimi Osterholm <jimi.osterholm@satel.com>
2021-12-02 14:08:43 +0200

    Some small changes to day1

--- day1/prog.py
+++ day1/prog.py
@@ -1,9 +1,7 @@
 import time
 def day_1_part_1():
     data = open("input.txt", "r")
-
-    lines = data.readlines()
-
+    lines = [int(i) for i in data.readlines()]
     temp = ""
     increased = 0
     for line in lines:
@@ -12,17 +10,13 @@ def day_1_part_1():
             temp = line
             continue
         else:
-            if int(line) > int(temp):
+            if line > temp:
                 increased += 1
-                temp = line
-            else:
-                temp = line
-                
-    #print("Measurements increased in {}".format(increased))
+            temp = line                
+    print("Measurements increased in {}".format(increased))
 
 def day_1_part_2():    
     data = open("input.txt", "r")
-
     lines = [int(i) for i in data.readlines()]
     increased = 0
     for line in range(len(lines)):
@@ -30,14 +24,7 @@ def day_1_part_2():
         data_2 = sum(lines[line+1:line+4])
         if data_1 < data_2:
             increased += 1
-    #print("Measurements increased in {}".format(increased))
+    print("Measurements increased in {}".format(increased))
 
-times = []
-for i in range(500):
-    start = time.time()
-    #day_1_part_1()
-    day_1_part_2()
-    end = time.time() - start
-    times.append(end)
-print("Time taken {}".format(sum(times) / len(times)))
-    
+day_1_part_1()
+day_1_part_2()
